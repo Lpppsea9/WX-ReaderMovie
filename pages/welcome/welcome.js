5efb2324bae19e0a139a7f450cab8bf1988cc7dd
Lpppsea9 <lpppsea9@foxmail.com>
2020-12-01 16:20:05 +0800

    12/1

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -3,9 +3,13 @@ Page({
     // wx.navigateTo({ //页面隐藏
     //   url: '../posts/posts',
     // })
-    wx.redirectTo({ //页面卸载，已经不存在了
+    // wx.redirectTo({ //页面卸载，已经不存在了
+    //   url: '../posts/posts',
+    // })
+    // 冒泡和非冒泡
+
+    wx.switchTab({
       url: '../posts/posts',
     })
-    // 冒泡和非冒泡
   },
 })
